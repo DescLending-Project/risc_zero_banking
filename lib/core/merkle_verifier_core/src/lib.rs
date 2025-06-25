3bb25e45d83a8f41b5593b34bbc0c3b21af4cc45
witja46 <witja46@gmail.com>
2025-06-25 23:08:22 +0200

    feut(merkle_verifier_core): Added utils for fetching of the required score calculation fields from the contract.

--- lib/core/merkle_verifier_core/src/lib.rs
+++ lib/core/merkle_verifier_core/src/lib.rs
@@ -1,1 +1,2 @@
+pub mod fetch_merkle_proofs;
 pub mod merkle_patricia;
--- lib/core/merkle_verifier_core/tests/patricia_test.rs
+++ lib/core/merkle_verifier_core/tests/patricia_test.rs
@@ -1,5 +1,12 @@
-#[test]
-fn test_basic_sanity() {
-    assert_eq!(2 + 2, 4);
-    println!("Basic sanity test in guest/src  passed!");
+#[cfg(test)]
+mod tests {
+    use ethers::{
+        core::types::{Address, EIP1186ProofResponse, H256, U256},
+        providers::{Http, Middleware, Provider},
+        utils::keccak256,
+    };
+    use merkle_verifier_core::fetch_merkle_proofs::*;
+    use std::{str::FromStr, vec};
+    #[tokio::test]
+    async fn verify_user_history_proof() {}
 }
